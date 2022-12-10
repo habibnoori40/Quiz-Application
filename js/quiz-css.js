5e84e67f0c049bb8c2f4d04e2ac023e4d8b6eda9
Habib Noori <84266368+habibnoori40@users.noreply.github.com>
2022-12-10 05:38:28 +0500

    Update quiz-css.js

--- js/quiz-css.js
+++ js/quiz-css.js
@@ -1,4 +1,4 @@
 import { quizStarter } from "../app.js";
 
-// quizStarter("../questions/css_mcq.json");
-quizStarter("https://api.npoint.io/1ec99a0b8a16cdcf2b29");
+quizStarter("../questions/css_mcq.json");
+// quizStarter("https://api.npoint.io/1ec99a0b8a16cdcf2b29");
