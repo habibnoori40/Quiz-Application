12ef5c3c22a074c2ee6b3c440d222f0e70081378
Habib Noori <84266368+habibnoori40@users.noreply.github.com>
2022-12-11 00:40:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -179,7 +179,7 @@ createStars();
 // Create Stars Image GIF
 function createStars() {
   let img = document.createElement("img");
-  if(location.pathname == "/quiz-application") {
+  if(location.pathname == "/quiz-application/") {
     img.src = "./img/stars.gif";
   } else {
     img.src = "../img/stars.gif";
