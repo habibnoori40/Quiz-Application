3444153fc121a95854599893c538972a9bcf0c24
Habib Noori <84266368+habibnoori40@users.noreply.github.com>
2022-12-10 05:38:03 +0500

    Update general.js

--- js/general.js
+++ js/general.js
@@ -1,4 +1,4 @@
 import { quizStarter } from "../app.js";
 
-// quizStarter("../questions/general_mcq.json");
-quizStarter("https://api.npoint.io/0280a83147f705ea143b");
+quizStarter("../questions/general_mcq.json");
+// quizStarter("https://api.npoint.io/0280a83147f705ea143b");
